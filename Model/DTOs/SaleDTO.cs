8ef28515eb468e141769012ba20df515a894e4ff
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2024-12-26 09:11:27 +0630

    -

--- Model/DTOs/SaleDTO.cs
+++ Model/DTOs/SaleDTO.cs
@@ -3,7 +3,7 @@
     public class CreateSaleDTO
     {
         public Guid ProductID { get; set; }
-        public int? QuantitySold { get; set; }
+        public int QuantitySold { get; set; }
         public string? CreatedBy { get; set; }
 
     }
--- Model/Entities/SaleReports.cs
+++ Model/Entities/SaleReports.cs
@@ -14,9 +14,9 @@ namespace MODEL.Entities
         [Key]
         public Guid SaleID { get; set; } = new Guid();
         public Guid ProductID { get; set; }
-        public int? QuantitySold { get; set; }
-        public decimal? TotalPrice { get; set; }
-        public decimal? TotalProfit { get; set; }
+        public int QuantitySold { get; set; }
+        public decimal TotalPrice { get; set; }
+        public decimal TotalProfit { get; set; }
         public DateTime? CreatedDate { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedDate { get; set; } = DateTime.UtcNow;
         public string? CreatedBy { get; set; }
