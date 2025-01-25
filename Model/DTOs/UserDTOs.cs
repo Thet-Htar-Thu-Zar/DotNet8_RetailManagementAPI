3c5893961e6660ffe7030d13ea9f2a504abe3661
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-25 20:55:04 +0630

    updateuserDTO

--- Model/DTOs/UserDTOs.cs
+++ Model/DTOs/UserDTOs.cs
@@ -14,4 +14,14 @@
         public string UserRole { get; set; }
         public string? CreatedBy { get; set; }
     }
+
+    public class UpdateUserDTOs
+    {
+        public Guid UserID { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public decimal? Amount { get; set; }
+        public string? UpdatedBy { get; set; }
+
+    }
 }
